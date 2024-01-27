31609731df6d269bb1867e369fa445551467cace
Anne L'Hôte <anne.lhote@gmail.com>
2024-01-27 22:19:40 +0100

    load all data from OverPass

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -37,52 +37,83 @@ const STYLE = {
   ]
 };
 
+const downSampleArray = (input, period) => {
+  if (period < 1 || period % 1 !== 0) {
+    throw new TypeError('Period must be an integer greater than or equal to 1')
+  }
+  if (period === 1) {
+    return [...input]
+  }
+  const output = []
+  for (let i = 0; i < input.length; i += period) {
+    output.push(input[i])
+  }
+  return output
+}
+
+const chunkArray = (array, chunkSize) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
+
+const getPointIcon = (amenity) => {
+  let icon = 'marker_11';
+  switch (amenity) {
+    case 'cafe':
+      icon = 'beer_11';
+      break;
+    case 'camp_site':
+      icon = 'campsite_11';
+      break;
+    case 'drinking_water':
+    case 'water_point':
+      icon = 'drinking_water_11';
+      break;
+    case 'hotel':
+      icon = 'lodging_11';
+      break;
+    case 'restaurant':
+      icon = 'restaurant_11';
+      break;
+    case 'toilets':
+      icon = 'toilet_11';
+      break;
+    default:
+      break;
+  }
+  return icon;
+}
+
+const getDataFromOverpass = (bbox) => {
+  const query = `
+    [out:json][timeout:500];
+    (
+      nwr["amenity"~"cafe|drinking_water|restaurant|toilets|water_point"](around:1000,${bbox});
+      nwr["tourism"~"camp_site|hotel"](around:1000,${bbox});
+    );
+    out center;
+  `;
+  return axios.get(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`);
+}
+
 function Map({ gpx }) {
   const [features, setFeatures] = useState([]);
-  const oneKmInDegree = 0.0089982311916;
-  const point = gpx.tracks[0].points[0];
-  const bbox = [point.lat - oneKmInDegree, point.lon - oneKmInDegree, point.lat + oneKmInDegree, point.lon + oneKmInDegree];
 
-  const getPointIcon = (amenity) => {
-    let icon = 'marker_11';
-    switch (amenity) {
-      case 'cafe':
-        icon = 'beer_11';
-        break;
-      case 'camp_site':
-        icon = 'campsite_11';
-        break;
-      case 'drinking_water':
-      case 'water_point':
-        icon = 'drinking_water_11';
-        break;
-      case 'hotel':
-        icon = 'lodging_11';
-        break;
-      case 'restaurant':
-        icon = 'restaurant_11';
-        break;
-      case 'toilets':
-        icon = 'toilet_11';
-        break;
-      default:
-        break;
-    }
-    return icon;
-  }
-
+  const coordinatesDataCount = gpx.tracks[0].points.length;
+  const targetPathDataCount = Math.pow(coordinatesDataCount, 0.7);
+  const pathSamplingPeriod = Math.floor(coordinatesDataCount / targetPathDataCount);
+  const downSampledCoordinates = downSampleArray(gpx.tracks[0].points, pathSamplingPeriod);
+  let chunks = chunkArray(downSampledCoordinates, 100);
+  chunks = chunks.map((chunk) => chunk.map((item) => [item.lat, item.lon]).flat());
+  
   useEffect(() => {
-    const query = `
-      [timeout:25][out:json][bbox:${bbox}];
-      (
-        nwr["amenity"~"cafe|drinking_water|restaurant|toilets|water_point"];
-        nwr["tourism"~"camp_site|hotel"];
-      );
-      out center;
-    `;
-    axios.get(`https://overpass-api.de/api/interpreter?data=${encodeURIComponent(query)}`)
-      .then((response) => {
-        setFeatures(response.data.elements.map((element) => ({
+    Promise.all(chunks.map((chunk) => getDataFromOverpass(chunk)))
+      .then((responses) => {
+        const response = responses.map((response) => response.data.elements).flat();
+        setFeatures(response.map((element) => ({
           type: 'Feature',
           properties: {
             description: element?.tags?.name ?? '',
@@ -97,8 +128,16 @@ function Map({ gpx }) {
       .catch(error => {
         console.log("error", error);
       });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  if (features.length === 0) {
+    return (
+      <div>
+        Loading ...
+      </div>
+    )
+  }
 
   return (
     <ReactMapGL
