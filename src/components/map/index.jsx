348f2e950c8ffd9c7c814104241a6671da358132
Anne L'Hôte <anne.lhote@gmail.com>
2024-03-10 17:26:31 +0100

    feat(gpx): Display each step in a different color

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,9 +1,11 @@
 import { Grid } from '@mui/material';
+import gpxParser from 'gpxparser';
 import maplibregl from 'maplibre-gl';
 import React, { useState } from 'react';
 import { default as ReactMapGL, FullscreenControl, Layer, NavigationControl, Source } from 'react-map-gl';
 
 import { CustomMarker, CustomPopup } from '../popup';
+import { getClosestPointIndexByDistance } from '../../utils';
 
 const mapStyle = {
   version: 8,
@@ -35,6 +37,21 @@ const Map = ({ coordinates, gpx, markers, meta, selectedFilters, setCoordinates 
   const closePopup = () => setSelectedMarker();
   const openPopup = (index) => setSelectedMarker(index);
 
+  // Calculate number of days in trail
+  const duration = Math.ceil(Math.floor(gpx.tracks[0].distance.total / 1000) / meta.kmPerDay)
+  const days = [...Array(duration).keys()];
+  // Determinates each step
+  const points = gpx.tracks[0].points;
+  const cumulDistances = [0, ...gpx.calculDistance(points).cumul.slice(0, -1)];
+  const gpxs = days.map((day) => {
+    const startPointIndex = getClosestPointIndexByDistance({ cumulDistances, distance: meta.kmPerDay * 1000 * day });
+    const endPointIndex = getClosestPointIndexByDistance({ cumulDistances, distance: meta.kmPerDay * 1000 * (day + 1) });
+    const trkpts = points.slice(startPointIndex, endPointIndex).map((point) => `<trkpt lat="${point.lat}" lon="${point.lon}"><ele>${point.ele}</ele></trkpt>`);
+    const newGpx = new gpxParser();
+    newGpx.parse(`<xml><gpx><trk><trkseg>${trkpts}</trkseg></trk></gpx></xml>`);
+    return newGpx;
+  });
+
   return (
     <Grid className="map" item xs={12}>
       <ReactMapGL
@@ -46,28 +63,31 @@ const Map = ({ coordinates, gpx, markers, meta, selectedFilters, setCoordinates 
         onMouseLeave={() => setCoordinates()}
         onMouseEnter={(e) => setCoordinates({ lat: e.lngLat.lat, lon: e.lngLat.lng })}
       >
-        <Source
-          data={gpx.toGeoJSON()}
-          type="geojson"
-        >
-          <Layer
-            id="path"
-            layout={{ "line-join": "round", "line-cap": "round" }}
-            paint={{ "line-color": '#10a4e5', "line-width": 3 }}
-            type="line"
-          />
-        </Source>
-        {coordinates && (
+        {gpxs.map((gpx, index) => (
           <Source
-            data={{ coordinates, type: "Point" }}
+            data={gpx.toGeoJSON()}
+            key={`gpx-${index}`}
             type="geojson"
           >
             <Layer
-              paint={{ "circle-color": "red", "circle-radius": 7 }}
-              type="circle"
+              id={`path-${index}`}
+              layout={{ "line-join": "round", "line-cap": "round" }}
+              paint={{ "line-color": index % 2 ? '#9A9EEC' : '#5756A6', "line-width": 3 }}
+              type="line"
             />
           </Source>
-        )}
+        ))}
+        {coordinates && (
+            <Source
+              data={{ coordinates, type: "Point" }}
+              type="geojson"
+            >
+              <Layer
+                paint={{ "circle-color": "red", "circle-radius": 7 }}
+                type="circle"
+              />
+            </Source>
+          )}
         {
           selectedMarker &&
           <CustomPopup
